27a8ec5f537e4347cb7184fbbabc766db78bc396
Andy Gabler <andrewpgabler@gmail.com>
2023-02-22 19:06:49 -0500

    13. Slow down the lasers (for now) to make the animation look better. we can tune this later.

--- src/com/andronikus/gameserver/engine/ScalableBalanceConstants.java
+++ src/com/andronikus/gameserver/engine/ScalableBalanceConstants.java
@@ -76,7 +76,7 @@ public class ScalableBalanceConstants {
     /*
      * Speed of a laser as it leaves a ship.
      */
-    public static final long LASER_SPEED = 59;
+    public static final long LASER_SPEED = 31;
 
     /*
      * Laser sizes.
